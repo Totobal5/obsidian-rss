594345951c168d4970f39f938f21cd4ade65d497
Totobal <cristobal.alarcon@posteo.net>
2025-08-16 23:58:47 -0400

    test(smoke): add static imports smoke test to catch missing mock exports early

---
+++ test/importsSmoke.test.ts
@@ -0,0 +1,26 @@
+// Static smoke test: if any of these throws on import, test fails early.
+import * as MainMod from '../src/main';
+import * as ViewLoaderMod from '../src/view/ViewLoader';
+import RssRoot from '../src/view/RssRoot.svelte';
+import * as FeedModalMod from '../src/modals/FeedModal';
+import * as ItemModalMod from '../src/modals/ItemModal';
+import * as TagModalMod from '../src/modals/TagModal';
+import * as SettingsTabMod from '../src/settings/SettingsTab';
+import * as ParserMod from '../src/parser/rssParser';
+import * as ProvidersMod from '../src/providers/Providers';
+import * as FeedUpdaterMod from '../src/services/FeedUpdater';
+
+describe('imports smoke', () => {
+  test('core modules import statically', () => {
+    expect(MainMod).toBeTruthy();
+    expect(ViewLoaderMod).toBeTruthy();
+    expect(RssRoot).toBeTruthy();
+    expect(FeedModalMod).toBeTruthy();
+    expect(ItemModalMod).toBeTruthy();
+    expect(TagModalMod).toBeTruthy();
+    expect(SettingsTabMod).toBeTruthy();
+    expect(ParserMod).toBeTruthy();
+    expect(ProvidersMod).toBeTruthy();
+    expect(FeedUpdaterMod).toBeTruthy();
+  });
+});
